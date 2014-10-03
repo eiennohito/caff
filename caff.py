bb4a8cabd12b4251f8c85630ec7a98f27a830c1a
Tolmachev Arseny <eiennohito@gmail.com>
2014-10-03 13:35:12 +0900

    added caff.py

---
+++ caff.py
@@ -0,0 +1,11 @@
+--2014-10-03 13:34:22--  https://gist.githubusercontent.com/eiennohito/10447343/raw/d3553b3683308ed4b3569ebda30d03ce8a800d68/caff
+Resolving gist.githubusercontent.com... 192.30.252.157
+Connecting to gist.githubusercontent.com|192.30.252.157|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: unspecified [text/plain]
+Saving to: 'caff'
+
+     0K                                                        11.5M=0s
+
+2014-10-03 13:34:23 (11.5 MB/s) - 'caff' saved [1000]
+
