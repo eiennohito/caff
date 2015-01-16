7b8da8324c64223e111215590ac509f13fc3a100
Arseny Tolmachev <eiennohito@gmail.com>
2015-01-16 16:19:42 +0900

    let user specify absolute time
    using format -t HH:MM[:ss]

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a wrapper around the caffeinate utility.
+It allows you to force your computer not to sleep (or use other options of `caffeinate`).
+However, it doesn't support running utilites, use original `caffeinate` for that.
--- caff.py
+++ caff.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
 
-from optparse import OptionParser
+from optparse import OptionParser, Option, OptionValueError
 import re
 from os import execvp
+from datetime import datetime, timedelta
+from copy import copy
 
-parser = OptionParser()
+def check_int(s):
+    try:
+        return int(s)
+    except TypeError:
+        return 0
 
+def check_time(option, opt, value):
+    m = re.match(r"(\d\d):(\d\d)(?::(\d\d))?", value)
+    if m is None:
+        raise OptionValueError("")
+    else:
+        return map(check_int, m.groups())
+
+class TimeOption(Option):
+    TYPES = Option.TYPES + ("time",)
+    TYPE_CHECKER = copy(Option.TYPE_CHECKER)
+    TYPE_CHECKER["time"] = check_time
+
+parser = OptionParser(option_class=TimeOption)
 parser.add_option('-i', action="store_const", dest='mode', const='i')
 parser.add_option('-m', action="store_const", dest='mode', const='m')
 parser.add_option('-s', action="store_const", dest='mode', const='s')
 parser.add_option('-u', action="store_const", dest='mode', const='u')
+parser.add_option('-t', type="time", dest="till")
 
 parser.set_default('mode', 'd')
 
@@ -25,15 +45,24 @@ multipliers = {
 }
 
 total = 0  # 0 seconds by default
+d = datetime.now()
+if opts.till is None:
+    for arg in args:
+      for pattern in re.findall(r"(?:(-?\d+(?:\.\d+)?)([smhd])?)", arg):
+        (dur, measure) = pattern
+        total += float(dur) * multipliers[measure]
+else:
+   time = opts.till
+   d2 = d.replace(hour=time[0], minute=time[1], second=time[2])
+   if d2 < d:
+       d2 = d2 + timedelta(days=1)
+   total = (d2 - d).total_seconds()
 
-for arg in args:
-  for pattern in re.findall(r"(?:(-?\d+(?:\.\d+)?)([smhd])?)", arg):
-    (dur, measure) = pattern
-    total += float(dur) * multipliers[measure]
 
 if (total == 0):
-  print "Specify duration in format 3.1d 1h5m 2s"
+    print "Specify duration in format 3.1d 1h5m 2s.\nOtherwise you can use -t 12:30[:15] format to sleep until 12:30.\nUtil will use 24h format and nearest time in the future."
 else:
-  print "Not sleeping for %d seconds" % total
+  end = datetime.now() + timedelta(seconds = total)
+  print "Not sleeping for %d seconds, until %s" % (total, end.strftime("%c"))
 
   execvp("/usr/bin/caffeinate", ["caffeinate", "-%s" % mode, "-t", str(int(total))])
